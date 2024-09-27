eee9094926be9f7ddb69ff85d207b22b0ccae6a4
Coders Club <hi@codersclub.com>
2024-09-27 17:42:36 +0300

    Lesson 02

--- app.js
+++ app.js
@@ -1,10 +1,26 @@
 const expres = require('express');
-
+const ejs=require("ejs")
 const app = expres();
-app.get('/', (req, res) => {
-  res.status(200).send('index');
-});
 
+//template engine
+app.set("view engine","ejs")
+
+
+//middleware
+app.use(expres.static("public"))
+
+app.get('/', (req, res) => {
+  res.status(200).render('index',{
+    Page_Name:"index"
+  });
+});
+app.get('/about', (req, res) => {
+    res.status(200).render('about',{
+      Page_Name:"about"
+    });
+  });
+
+  
 const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`App started on port ${PORT}`);
