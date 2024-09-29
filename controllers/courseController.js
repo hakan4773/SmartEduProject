9902d107eaa49b56fe546264a0306bd46cbf4a62
hakan4773 <hb480312@gmail.com>
2024-09-29 16:42:56 +0300

    Lesson 05

--- controllers/courseController.js
+++ controllers/courseController.js
@@ -1,8 +1,9 @@
 const Course=require("../models/Course")
-
+const Category=require("../models/Category")
 exports.createCourse= async (req,res)=>{
 
 try {
+
     const course =await Course.create(req.body)
     res.status(201).json({
         status: "success",
@@ -19,9 +20,20 @@ try {
 }
 exports.getAllCourse= async (req,res)=>{
 try {
-    const courses=await Course.find({}).sort("-createdAt")
+    const categoryslug=req.query.categories
+     const category=await Category.findOne({slug:categoryslug})
+
+let filter={}
+if(categoryslug){
+    filter={category:category._id}
+}
+   
+
+const courses=await Course.find(filter).sort("-createdAt") 
+const categories=await Category.find()
 res.status(200).render("courses",{
     courses,
+    categories,
     Page_Name:"courses"
 })
 } catch (error) {
