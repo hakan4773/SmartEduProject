b6490ab122b38149e4f2e3879ed1b9c3bc501848
hakan4773 <hb480312@gmail.com>
2024-10-02 15:20:41 +0300

    lesson 08

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,7 @@
-const User = require('../models/User');
 const bcrypt = require('bcrypt');
+const User = require('../models/User');
+const Category=require("../models/Category");
+const Course = require('../models/Course');
 exports.createUser = async (req, res) => {
   try {
     const user = await User.create(req.body);
@@ -39,10 +41,22 @@ exports.logoutUser = (req, res) => {
 
 exports.getDashboard = async (req, res) => {
   try {
+<<<<<<< HEAD
     const user = await User.findOne({ _id: req.session.userID });
     res.status(200).render('dashboard', {
       Page_Name: 'dashboard',
       user,
+=======
+    const user = await User.findOne({ _id: req.session.userID }); 
+    const categories=await Category.find()
+    const courses=await Course.find({user:req.session.userID})
+
+    res.status(200).render('dashboard', {
+      Page_Name: 'dashboard',
+      user,
+      categories,
+      courses
+>>>>>>> parent of 6f5c1b0 (Revert "lesson 08")
     });
   } catch (error) {
     res.status(400).json({
--- routes/courseRoute.js
+++ routes/courseRoute.js
@@ -1,6 +1,9 @@
 const express=require("express")
 const courseController=require("../controllers/courseController")
+<<<<<<< HEAD
 const middleWears=require("../Middlewares/roleMiddlewears");
+=======
+>>>>>>> parent of 6f5c1b0 (Revert "lesson 08")
 const roleMiddlewears = require("../Middlewares/roleMiddlewears");
 const router=express.Router();
 
