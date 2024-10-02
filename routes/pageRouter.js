6f5c1b086beff1283ec08c2ff05bed5b818840e0
Coders Club <hi@codersclub.com>
2024-10-02 15:05:37 +0300

    Revert "lesson 08"
    This reverts commit b19d1b3c1314c3570593e7df58e9206d1e72bf24.

--- routes/pageRouter.js
+++ routes/pageRouter.js
@@ -1,13 +1,13 @@
 const express = require('express');
+
 const pageController=require("../controllers/pageController");
-const redirectMiddlewears=require("../Middlewares/redirectMiddlewears")
 
 const router=express.Router();
 
 router.route("/").get(pageController.getİndexPge)
 router.route("/about").get(pageController.getAboutPage)
-router.route("/register").get(redirectMiddlewears,pageController.getRegisterPage)
-router.route("/login").get(redirectMiddlewears,pageController.getLoginPage)
+router.route("/register").get(pageController.getRegisterPage)
+router.route("/login").get(pageController.getLoginPage)
 
 
 module.exports=router;
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,12 +1,11 @@
 const express=require("express")
 const authController=require("../controllers/authController")
-const authMiddlewares=require("../Middlewares/authMidlewares")
 
 const router=express.Router();
 
 router.route("/signup").post(authController.createUser)
 router.route("/login").post(authController.LoginUser)
 router.route("/logout").get(authController.logoutUser)
-router.route("/dashboard").get(authMiddlewares,authController.getDashboard)
+router.route("/dashboard").get(authController.getDashboard)
 
 module.exports=router;
